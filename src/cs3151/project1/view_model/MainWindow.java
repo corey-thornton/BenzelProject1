78bb0a7e1f207b1d02ca5d09b0c40ff5372f27fb
Corey Thornton <cthornt8@my.westga.edu>
2018-01-23 11:18:27 -0500

    Trying To fix GUI

--- src/cs3151/project1/view_model/MainWindow.java
+++ src/cs3151/project1/view_model/MainWindow.java
@@ -1,5 +1,8 @@
 package cs3151.project1.view_model;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
@@ -9,55 +12,64 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 
 public class MainWindow {
-	@FXML
-	private AnchorPane anchor;
 
-	@FXML
-	private Button directoryChooser;
+    @FXML
+    private ResourceBundle resources;
 
-	@FXML
-	private TextArea displayText;
+    @FXML
+    private URL location;
 
-	@FXML
-	private TextField patternBox;
+    @FXML
+    private AnchorPane anchor;
 
-	@FXML
-	private RadioButton selectAll;
+    @FXML
+    private Button directoryChooser;
 
-	@FXML
-	private ToggleGroup group1;
+    @FXML
+    private TextArea displayText;
 
-	@FXML
-	private RadioButton selectDirectories;
+    @FXML
+    private TextField patternBox;
 
-	@FXML
-	private RadioButton selectFiles;
+    @FXML
+    private RadioButton selectAll;
 
-	@FXML
-	private RadioButton nameOnly;
+    @FXML
+    private ToggleGroup group1;
 
-	@FXML
-	private ToggleGroup group2;
+    @FXML
+    private RadioButton selectDirectories;
 
-	@FXML
-	private RadioButton fullPath;
-	
-	
+    @FXML
+    private RadioButton selectFiles;
 
-	@FXML
-	void initialize() {
-		assert anchor != null : "fx:id=\"anchor\" was not injected: check your FXML file 'mainWindow.fxml'.";
-		assert directoryChooser != null : "fx:id=\"directoryChooser\" was not injected: check your FXML file 'mainWindow.fxml'.";
-		assert displayText != null : "fx:id=\"displayText\" was not injected: check your FXML file 'mainWindow.fxml'.";
-		assert patternBox != null : "fx:id=\"patternBox\" was not injected: check your FXML file 'mainWindow.fxml'.";
-		assert selectAll != null : "fx:id=\"selectAll\" was not injected: check your FXML file 'mainWindow.fxml'.";
-		assert group1 != null : "fx:id=\"group1\" was not injected: check your FXML file 'mainWindow.fxml'.";
-		assert selectDirectories != null : "fx:id=\"selectDirectories\" was not injected: check your FXML file 'mainWindow.fxml'.";
-		assert selectFiles != null : "fx:id=\"selectFiles\" was not injected: check your FXML file 'mainWindow.fxml'.";
-		assert nameOnly != null : "fx:id=\"nameOnly\" was not injected: check your FXML file 'mainWindow.fxml'.";
-		assert group2 != null : "fx:id=\"group2\" was not injected: check your FXML file 'mainWindow.fxml'.";
-		assert fullPath != null : "fx:id=\"fullPath\" was not injected: check your FXML file 'mainWindow.fxml'.";
+    @FXML
+    private RadioButton nameOnly;
 
-	}
+    @FXML
+    private ToggleGroup group2;
 
+    @FXML
+    private RadioButton fullPath;
+
+    @FXML
+    void onDirectoryClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert anchor != null : "fx:id=\"anchor\" was not injected: check your FXML file 'mainWindow.fxml'.";
+        assert directoryChooser != null : "fx:id=\"directoryChooser\" was not injected: check your FXML file 'mainWindow.fxml'.";
+        assert displayText != null : "fx:id=\"displayText\" was not injected: check your FXML file 'mainWindow.fxml'.";
+        assert patternBox != null : "fx:id=\"patternBox\" was not injected: check your FXML file 'mainWindow.fxml'.";
+        assert selectAll != null : "fx:id=\"selectAll\" was not injected: check your FXML file 'mainWindow.fxml'.";
+        assert group1 != null : "fx:id=\"group1\" was not injected: check your FXML file 'mainWindow.fxml'.";
+        assert selectDirectories != null : "fx:id=\"selectDirectories\" was not injected: check your FXML file 'mainWindow.fxml'.";
+        assert selectFiles != null : "fx:id=\"selectFiles\" was not injected: check your FXML file 'mainWindow.fxml'.";
+        assert nameOnly != null : "fx:id=\"nameOnly\" was not injected: check your FXML file 'mainWindow.fxml'.";
+        assert group2 != null : "fx:id=\"group2\" was not injected: check your FXML file 'mainWindow.fxml'.";
+        assert fullPath != null : "fx:id=\"fullPath\" was not injected: check your FXML file 'mainWindow.fxml'.";
+
+    }
 }
